f845915f74797252d6fe656de2062c3183383e6f
reynaraalbert <reynaraalbertpradana@gmail.com>
2026-08-12 21:17:39 +0700

    Auto-sanitize MONGO_URI string in server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/content', require('./routes/content'));
 
 // Database Connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI ? process.env.MONGO_URI.trim().replace(/^["']|["']$/g, '') : '';
 
 // Start Server
 app.listen(PORT, () => {
